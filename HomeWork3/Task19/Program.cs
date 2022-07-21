d11d70fcb3fe9d9fd6c7ed7c99761cec7a03ccb6
dmitrykislidenko <prince.Rassia@yandex.ru>
2022-07-21 10:10:22 +0300

    добавил второй вариант решения задачи

--- HomeWork3/Task19/Program.cs
+++ HomeWork3/Task19/Program.cs
@@ -1,24 +1,46 @@
 ﻿// Задача 19: Условия: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 // Палиндром -  число, буквосочетание, слово или текст, одинаково читающееся в обоих направлениях.
 // 14212 -> нет
-// 23432 -> да
 // 12821 -> да
+// 23432 -> да
+
+// Console.Clear();
+// Console.WriteLine("Введите пятизначное число: ");
+// string? number = Console.ReadLine();
+// void inputNumber(string number)
+// {
+//     if (number[0] == number[4])
+//     {
+//         if (number[1] == number[3])
+//             Console.WriteLine($"Введённое число: {number} -> да (палиндром).");
+//     } 
+//      else Console.WriteLine($"Введённое число: {number} -> нет (не палиндром).");
+
+// }
+// if (number!.Length == 5)
+// {
+//     inputNumber(number);
+// }
+// else Console.WriteLine($"Введёное число не пятизначное, введите корретное число:");
+
+
+// Второй вариант решения
 
 Console.Clear();
 Console.WriteLine("Введите пятизначное число: ");
 string? number = Console.ReadLine();
-
-void inputNumber(string number)
+while (number!.Length == 5)
 {
-    if (number[0] == number[4] || number[1] == number[3])
+    if (number[0] == number[4])
     {
-        Console.WriteLine($"Введённое число: {number} -> да (палиндром).");
+        if (number[1] == number[3])
+            Console.WriteLine($"Введённое число: {number} -> да (палиндром).");
     }
     else Console.WriteLine($"Введённое число: {number} -> нет (не палиндром).");
+    break;
 }
 
-if (number!.Length == 5)
 {
-    inputNumber(number);
+    if (number!.Length < 5 || number!.Length > 5)
+        Console.WriteLine($"Введёное число не пятизначное, введите корретное число:");
 }
-else Console.WriteLine($"Введёное число не пятизначное, введите корретное число");
