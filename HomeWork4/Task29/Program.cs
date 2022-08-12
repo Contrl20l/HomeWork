fb42ddebe2cf7eb870169cfc5a4005c59afe2f19
dmitrykislidenko <prince.Rassia@yandex.ru>
2022-08-12 13:20:15 +0300

    Решение 29 задачи

---
+++ HomeWork4/Task29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+Console.WriteLine("Созданый массив из 8 чисел: ");
+Console.Write("[");
+int[] numbers = new int[8];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+  numbers[i] = new Random().Next(0, 100);
+   if (i !=7)
+   Console.Write(numbers[i] + ", ");
+   else
+   Console.Write(numbers[i]);
+}
+
+Console.Write("]");
+
