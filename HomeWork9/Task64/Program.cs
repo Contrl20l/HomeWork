e702a91b2f084669f275b12a11990e5f673b58bf
dmitrykislidenko <prince.Rassia@yandex.ru>
2022-09-09 16:46:00 +0300

    Решение 64 задачи

--- HomeWork9/Task64/Program.cs
+++ HomeWork9/Task64/Program.cs
@@ -1,27 +1,28 @@
-﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
-// N = 5 -> "5, 4, 3, 2, 1"
-// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
 
 Console.Clear();
+int m = InputNumbers("Введите M: ");
 int n = InputNumbers("Введите N: ");
-int count = 2;
-PrintNumber(n, count);
-Console.Write(1);
 
+PrintNumber(m, n);
 
-// метод счёта и вывода значений
-void PrintNumber(int n, int count)
+// Метод расчёта и вывода ответа
+void PrintNumber(int m, int n)
 {
-  if (count > n) return;
-  PrintNumber(n, count + 1);
-  Console.Write(count + ", ");
+    for (int i = m; i <= n; i++)
+        if(i != n) 
+            Console.Write($"{i}, ");
+        else
+            Console.Write($"{i}.");
+        
 }
 
-
-//метод ввода значения с клавиатуры
-int InputNumbers(string input) 
+// Метод ввода
+int InputNumbers(string input)
 {
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
 }
